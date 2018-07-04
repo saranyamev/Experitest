e34abde5467a34ec038f2cab634cb9f10656412e
Saranya <Saranya@192.168.11.192>
2018-07-04 14:12:32 +0400

    Updated Config

--- src/test/java/Seetest/SampleiOSTest.java
+++ src/test/java/Seetest/SampleiOSTest.java
@@ -15,7 +15,7 @@ import java.util.logging.Level;
 public class SampleiOSTest {
     private String reportDirectory = "reports";
     private String reportFormat = "xml";
-    private String testName = "Untitled";
+    private String testName = "iOS Test";
     private String accessKey = "eyJ4cC51IjoxNDUxNDUsInhwLnAiOjE0NTE0NCwieHAubSI6Ik1UVXpNRFl4TXpZd01ESTJOQSIsImFsZyI6IkhTMjU2In0.eyJleHAiOjE4NDU5NzM2MDAsImlzcyI6ImNvbS5leHBlcml0ZXN0In0.KTQcweVJFYfTyQpITDV9kLv2-oQ7COnK5NB0Y9LK1Rs";
     protected IOSDriver<IOSElement> driver = null;
 
